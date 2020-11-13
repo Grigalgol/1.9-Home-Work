ec536d578e62b0ace72b4fff3f3a7b46143dc372
Grigoriy <grisha101202@yandex,ru>
2020-11-13 09:39:39 +0300

    ended 1

--- src/One.java
+++ src/One.java
@@ -1,7 +1,13 @@
 import java.util.*;
 public class One {
+    public static int sum(int x) {
+        return x%10+x/10;
+    }
+
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
-        
+        int n = in.nextInt();
+        int a = sum(n);
+        System.out.println(a);
     }
 }

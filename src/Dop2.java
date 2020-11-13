bcc2c86d2b4bb1dceaf06067a5562112a69a09e3
Grigoriy <grisha101202@yandex,ru>
2020-11-13 10:52:41 +0300

    ended dop 2

--- src/Dop2.java
+++ src/Dop2.java
@@ -1,12 +1,16 @@
 import java.util.*;
 public class Dop2 {
-    public static double s (int x, int y, int a, int b) {
-        return Math.sqrt((a-x)*(a-x)+(b-y)*(b-y));
+    public static int min (int a, int b, int c, int d) {
+        int minn= Math.min(a, b);
+        minn = Math.min(c, minn);
+        minn = Math.min(d, minn);
+        return minn;
+
     }
 
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
-
-
+        int a1=in.nextInt(), b1=in.nextInt(), c1=in.nextInt(), d1=in.nextInt();
+        System.out.println(min(a1,b1,c1,d1));
     }
 }
